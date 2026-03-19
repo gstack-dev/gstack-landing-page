2d6b9c7c97b0cda5a6478faf5880a1893d2f3af5
G-Stack <george.gstack@gmail.com>
2026-03-19 23:03:31 +0200

    Update Technologies.tsx

--- src/Sections/Technologies.tsx
+++ src/Sections/Technologies.tsx
@@ -4,7 +4,7 @@ const Technologies = () => {
             <h2 className="text-2xl lg:text-5xl font-bold md:text-4xl text-primary">Technologies</h2>
             <div className="flex flex-col w-full">
                 <div className="text-xl lg:text-2xl font-bold md:text-xl text-secondary mb-2 after:content-[''] after:block after:w-full after:h-0.5 after:bg-[#eee] after:rounded-lg">Frontend</div>
-                <div className="flex gap-10 text-black flex-wrap">
+                <div className="flex gap-7 text-black flex-wrap">
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">React</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">Next.js</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">TypeScript</div>
@@ -13,7 +13,7 @@ const Technologies = () => {
             </div>
             <div className="flex flex-col w-full flex-wrap">
                 <div className="text-xl lg:text-2xl font-bold md:text-xl text-secondary mb-2 after:content-[''] after:block after:w-full after:h-0.5 after:bg-[#eee] after:rounded-lg">Backend</div>
-                <div className="flex gap-10 text-black">
+                <div className="flex gap-7 text-black">
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">Node.js</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">Express.js</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">Laravel</div>
@@ -21,7 +21,7 @@ const Technologies = () => {
             </div>
             <div className="flex flex-col w-full flex-wrap">
                 <div className="text-xl lg:text-2xl font-bold md:text-xl text-secondary mb-2 after:content-[''] after:block after:w-full after:h-0.5 after:bg-[#eee] after:rounded-lg">Database</div>
-                <div className="flex gap-10 text-black">
+                <div className="flex gap-7 text-black">
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">MongoDB</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">SQL</div>
                     <div className="p-5 bg-background rounded-lg hover:bg-primary transition-all duration-300 cursor-pointer hover:text-accent">MySQL</div>
