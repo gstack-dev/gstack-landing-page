e1f5c44372969b462a954517b2dc38431ccefe45
george-iot <georgeshenoda@protonmail.com>
2026-03-19 22:46:41 +0200

    insights

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import AboutMe from "./Sections/AboutMe";
 import Services from "./Sections/Services";
 import Technologies from "./Sections/Technologies";
 import Portfolio from "./Sections/Portfolio";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
         <>
             <div className="text-white font-[poppins]">
+                <Analytics />
+                <SpeedInsights />
                 <Hero />
                 <Services />
                 <Technologies />
